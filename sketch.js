a78dd5120bf3adab8fa1ee7a4de92d81f5ddf5dc
LawnGeneral <steve.decker82@gmail.com>
2026-01-22 18:48:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -151,7 +151,25 @@ function getOverpassData() { //load nodes and edge map data in XML format from O
 	datamaxlat = extent[3] - (extent[3] - extent[1]) * margin;
 	datamaxlon = extent[2] - (extent[2] - extent[0]) * margin;
 	let OverpassURL = "https://overpass-api.de/api/interpreter?data=";
-	let overpassquery = "(way({{bbox}})['highway']['highway' !~ 'trunk']['highway' !~ 'motorway']['highway' !~ 'motorway_link']['highway' !~ 'raceway']['highway' !~ 'proposed']['highway' !~ 'construction']['highway' !~ 'service']['highway' !~ 'elevator']['footway' !~ 'crossing']['footway' !~ 'sidewalk']['foot' !~ 'no']['access' !~ 'private']['access' !~ 'no'];node(w)({{bbox}}););out;";
+	let overpassquery =
+  "(" +
+  "way({{bbox}})" +
+  "['highway']" +
+  "['name']" + // require street name (CityStrides-style)
+  "['highway'!~'^(bridleway|bus_guideway|busway|construction|corridor|cycleway|elevator|escape|footway|motorway|motorway_link|path|platform|proposed|raceway|razed|rest_area|services|steps|trunk|trunk_link|via_ferrata)$']" +
+  "['access'!~'^(no|customers|permit|private)$']" +
+  "['indoor'!~'^(area|column|corridor|door|level|room|wall|yes)$']" +
+  "['service'!~'^(drive-through|driveway|parking_aisle)$']" +
+  "['expressway'!~'^(yes)$']" +
+  "['fee'!~'^(yes)$']" +
+  "['foot'!~'^(no)$']" +
+  "['motorroad'!~'^(yes)$']" +
+  "['area'!~'^(yes)$']" +
+  "['public_transport'!~'^(platform)$']" +
+  "['toll'!~'^(yes)$'];" +
+  "node(w)({{bbox}});" +
+  ");out;";
+
 
 	overpassquery = overpassquery.replace("{{bbox}}", dataminlat + "," + dataminlon + "," + datamaxlat + "," + datamaxlon);
 	overpassquery = overpassquery.replace("{{bbox}}", dataminlat + "," + dataminlon + "," + datamaxlat + "," + datamaxlon);
