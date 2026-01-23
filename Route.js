8e1ab88a511aa9b1707ddef35f2ab92995a0e5f6
LawnGeneral <steve.decker82@gmail.com>
2026-01-23 15:17:40 -0500

    Update Route.js

--- Route.js
+++ Route.js
@@ -1,19 +1,16 @@
 class Route {
     constructor(startnode, originalroute) {
-        if (originalroute == null) { // create a new route with just a node
+        if (originalroute == null) { 
             this.waypoints = [];
             this.minlat = Infinity;
             this.maxlat = 0;
             this.minlon = Infinity;
             this.maxlon = 0;
-            this.waypoints.push(startnode);
+            if (startnode) this.waypoints.push(startnode);
             this.distance = 0;
             this.doublingsup = 0;
-        } else { // make a deep copy of a route
-            this.waypoints = [];
-            for (let i = 0; i < originalroute.waypoints.length; i++) {
-                this.waypoints.push(originalroute.waypoints[i]);
-            }
+        } else { 
+            this.waypoints = [...originalroute.waypoints];
             this.minlat = originalroute.minlat;
             this.maxlat = originalroute.maxlat;
             this.minlon = originalroute.minlon;
@@ -23,84 +20,47 @@ class Route {
         }
     }
 
-    addWaypoint(node, dist, doublingsup) {
+    addWaypoint(node, dist, doublingsup = 0) {
         this.waypoints.push(node);
         this.distance += dist;
+        this.doublingsup += doublingsup;
         this.minlat = min(this.minlat, node.lat);
         this.maxlat = max(this.maxlat, node.lat);
         this.minlon = min(this.minlon, node.lon);
         this.maxlon = max(this.maxlon, node.lon);
-        this.doublingsup += doublingsup;
-    }
-
-    area() { // rough approximation of the area
-        return (this.maxlat - this.minlat) * (this.maxlon - this.minlon);
     }
 
     show() {
-        stroke(255, 255, 255, 55);
+        if (this.waypoints.length < 2) return;
+        push();
+        noFill();
         strokeWeight(5);
         for (let i = 0; i < this.waypoints.length - 1; i++) {
-            let from = createVector(this.waypoints[i].x, this.waypoints[i].y);
-            let to = createVector(this.waypoints[i + 1].x, this.waypoints[i + 1].y);
-            let vline = p5.Vector.sub(to, from);
+            let from = this.waypoints[i];
+            let to = this.waypoints[i + 1];
             let hue = map(i, 0, this.waypoints.length - 1, 0, 155);
-            stroke(hue, 255, 255, 0.5);
+            stroke(hue, 255, 255, 0.6);
             line(from.x, from.y, to.x, to.y);
-            if (showSteps) {
-                fill(hue, 255, 55, 0.8);
-                noStroke();
-                textSize(8);
-                let textangle = vline.heading() + HALF_PI;
-                text(i, 10 * cos(textangle) + (from.x + to.x) / 2 - 5, 10 * sin(textangle) + (from.y + to.y) / 2);
-            }
-
         }
-        noStroke();
-        if (this.waypoints.length > 0) {
-            fill(0, 255, 255, 0.8);
-            ellipse(this.waypoints[0].x, this.waypoints[0].y, 20, 20); //show the first waypoint
-        }
-        fill(149, 255, 255, 0.8);
-        ellipse(this.waypoints[bestroute.waypoints.length - 1].x, this.waypoints[bestroute.waypoints.length - 1].y, 20, 20); //show the last waypoint
+        
+        // Mark Start (Yellow) and End (Cyan)
+        fill(60, 255, 255); // Yellow
+        ellipse(this.waypoints[0].x, this.waypoints[0].y, 15, 15);
+        fill(180, 255, 255); // Cyan
+        ellipse(this.waypoints[this.waypoints.length - 1].x, this.waypoints[this.waypoints.length - 1].y, 10, 10);
+        pop();
     }
 
     exportGPX() {
-        let xmlDoc = document.implementation.createDocument(null, "gpx");
-        let x = xmlDoc.getElementsByTagName('gpx');
-        x[0].setAttribute("xmlns:gpxtpx", "http://www.garmin.com/xmlschemas/TrackPointExtension/v1");
-        x[0].setAttribute("version", "1.1");
-        x[0].setAttribute("xm1ns", "http://www.topografix.com/GPX/1/1"); // for some weird reason the XML DOM won't add an attribute called exactly 'xmlns', so I'm fudging it and replacing it later.
-        let trk = xmlDoc.createElement("trk");
-        let trknumber = xmlDoc.createElement("type");
-        trknumber.appendChild(xmlDoc.createTextNode("9"));
-        trk.appendChild(trknumber);
-        let trkseg = xmlDoc.createElement("trkseg");
-
+        let xmlString = '<?xml version="1.0" encoding="UTF-8"?>\n';
+        xmlString += '<gpx version="1.1" creator="GeminiRoute" xmlns="http://www.topografix.com/GPX/1/1">\n';
+        xmlString += '  <trk><name>Every Single Street</name><trkseg>\n';
         let now = new Date();
-
-        for (let i = 0; i < this.waypoints.length; i++) {
-            let trkpt = xmlDoc.createElement("trkpt");
-            trkpt.setAttribute("lat", this.waypoints[i].lat);
-            trkpt.setAttribute("lon", this.waypoints[i].lon);
-            let ele = xmlDoc.createElement("ele");
-            ele.appendChild(xmlDoc.createTextNode("2000"));
-            trkpt.appendChild(ele);
-            let timenode = xmlDoc.createElement("time");
-            let fakedDate = new Date(now.getFullYear(), now.getMonth(), now.getDate()-1, now.getHours(), now.getMinutes() + i); // create a fake timestamp for the route by adding 1 min for each waypoint
-            timenode.appendChild(xmlDoc.createTextNode(fakedDate.toISOString()));
-            trkpt.appendChild(timenode);
-            trkseg.appendChild(trkpt);
-        }
-
-        trk.appendChild(trkseg);
-        xmlDoc.documentElement.appendChild(trk);
-
-        var serializer = new XMLSerializer();
-        let XMLString = serializer.serializeToString(xmlDoc)
-        let XMLwriter = createWriter('route.gpx');
-        XMLString = XMLString.replace('xm1ns','xmlns'); // for some weird reason the XML DOM won't add an attribute called exactly 'xmlns', so I'm fudging it.
-        XMLwriter.write(XMLString);
-        XMLwriter.close();
+        this.waypoints.forEach((pt, i) => {
+            let time = new Date(now.getTime() + i * 10000).toISOString();
+            xmlString += `    <trkpt lat="${pt.lat}" lon="${pt.lon}"><ele>0</ele><time>${time}</time></trkpt>\n`;
+        });
+        xmlString += '  </trkseg></trk>\n</gpx>';
+        saveStrings([xmlString], 'route.gpx');
     }
 }
