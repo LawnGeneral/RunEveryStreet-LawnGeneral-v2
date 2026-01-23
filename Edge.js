1c2dd55ba45cb87310dd87017b76fdf904d57fab
LawnGeneral <steve.decker82@gmail.com>
2026-01-23 12:43:46 -0500

    Update Edge.js

--- Edge.js
+++ Edge.js
@@ -1,4 +1,4 @@
-class Edge { //section of road that connects nodes
+class Edge { 
 	constructor(from_, to_, wayid_) {
 		this.wayid = wayid_;
 		this.from = from_;
@@ -14,19 +14,25 @@ class Edge { //section of road that connects nodes
 	}
 
 	show() {
-		strokeWeight(min(10, (this.travels + 1) * 2));
-		stroke(55, 255, 255, 0.8);
-		line(this.from.x, this.from.y, this.to.x, this.to.y);
-		fill(0);
-		noStroke();
+		let p1 = this.from.getScreenPos();
+		let p2 = this.to.getScreenPos();
+
+		if (p1 && p2) {
+			strokeWeight(min(10, (this.travels + 1) * 2));
+			stroke(55, 255, 255, 0.8);
+			line(p1.x, p1.y, p2.x, p2.y);
+		}
 	}
 
 	highlight() {
-		strokeWeight(4);
-		stroke(20, 255, 255, 1);
-		line(this.from.x, this.from.y, this.to.x, this.to.y);
-		fill(0);
-		noStroke();
+		let p1 = this.from.getScreenPos();
+		let p2 = this.to.getScreenPos();
+
+		if (p1 && p2) {
+			strokeWeight(6); // Slightly thicker for easier visibility
+			stroke(20, 255, 255, 1);
+			line(p1.x, p1.y, p2.x, p2.y);
+		}
 	}
 
 	OtherNodeofEdge(node) {
@@ -37,9 +43,15 @@ class Edge { //section of road that connects nodes
 		}
 	}
 
-	distanceToPoint(x,y) {  //distance from middle of this edge to give point
-		return(dist(x,y,(this.to.x+this.from.x)/2,(this.to.y+this.from.y)/2));
+	// Dynamic distance check based on current screen pixels
+	distanceToPoint(x, y) {
+		let p1 = this.from.getScreenPos();
+		let p2 = this.to.getScreenPos();
+		
+		if (p1 && p2) {
+			// Measures distance from mouse to the midpoint of the road on screen
+			return dist(x, y, (p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+		}
+		return Infinity;
 	}
-
-
 }
