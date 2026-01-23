2ea19f448adc8d0ea15bb5c9d559d71def8dfee7
LawnGeneral <steve.decker82@gmail.com>
2026-01-23 12:28:37 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -136,76 +136,109 @@ function draw() { //main loop called by the P5.js framework every frame
 	}
 }
 
-function getOverpassData() { //load nodes and edge map data in XML format from OpenStreetMap via the Overpass API
-	showMessage("Loading map data…");
-	canvas.position(0, 34); // start canvas just below logo image
-	bestroute = null;
-	totaluniqueroads=0;
-	var extent = ol.proj.transformExtent(openlayersmap.getView().calculateExtent(openlayersmap.getSize()), 'EPSG:3857', 'EPSG:4326'); //get the coordinates current view on the map
-	mapminlat = extent[1];
-	mapminlon = extent[0];
-	mapmaxlat = extent[3];
-	mapmaxlon = extent[2]; //51.62354589659512,0.3054885475158691,51.635853268644496,0.33291145248413084
-	dataminlat = extent[1] + (extent[3] - extent[1]) * margin; //51.62662273960746,0.31234427375793455,51.63277642563215,0.3260557262420654
-	dataminlon = extent[0] + (extent[2] - extent[0]) * margin;
-	datamaxlat = extent[3] - (extent[3] - extent[1]) * margin;
-	datamaxlon = extent[2] - (extent[2] - extent[0]) * margin;
-	let OverpassURL = "https://overpass-api.de/api/interpreter?data=";
-	let overpassquery = `
-  [out:xml][timeout:25];
-  (
-    way["highway"]["name"]({{bbox}})
-    ["highway"!~"^(motorway|motorway_link|service|bridleway|bus_guideway|busway|construction|corridor|cycleway|elevator|escape|footway|path|platform|proposed|raceway|razed|rest_area|services|steps|via_ferrata)$"]
-    ["access"!~"^(no|private)$"]
-    ["area"!~"yes"];
-  );
-  (._;>;);
-  out;`;
+function getOverpassData() { 
+    showMessage("Loading map data…");
+    canvas.position(0, 34); 
+    bestroute = null;
+    totaledgedistance = 0;
+    showRoads = true;
+    totaluniqueroads = 0;
 
-	overpassquery = overpassquery.replace("{{bbox}}", dataminlat + "," + dataminlon + "," + datamaxlat + "," + datamaxlon);
-	overpassquery = overpassquery.replace("{{bbox}}", dataminlat + "," + dataminlon + "," + datamaxlat + "," + datamaxlon);
-	OverpassURL = OverpassURL + encodeURI(overpassquery);
-	httpGet(OverpassURL + encodeURIComponent(query), 'text', true, function(response) {
-    var parser = new DOMParser();
-    let xml = parser.parseFromString(response, "text/xml");
-    let xmlWays = xml.getElementsByTagName("way");
+    // Get the coordinates from the current map view
+    var extent = ol.proj.transformExtent(openlayersmap.getView().calculateExtent(openlayersmap.getSize()), 'EPSG:3857', 'EPSG:4326');
     
-    // SAFETY CHECK: If no named roads are found, don't freeze!
-    if (xmlWays.length === 0) {
-        showMessage("No named roads found here. Try zooming out.");
-        setTimeout(() => { 
-            mode = choosemapmode; 
-            showMessage("Zoom to selected area, then click here");
-        }, 3000);
-        return;
-    }
-	
-			nodes = [];
-		edges = [];
-		for (let i = 0; i < numnodes; i++) {
-			var lat = XMLnodes[i].getAttribute('lat');
-			var lon = XMLnodes[i].getAttribute('lon');
-			var nodeid = XMLnodes[i].getAttribute('id');
-			let node = new Node(nodeid, lat, lon);
-			nodes.push(node);
-		}
-		//parse ways into edges
-		for (let i = 0; i < numways; i++) {
-			let wayid = XMLways[i].getAttribute('id');
-			let nodesinsideway = XMLways[i].getElementsByTagName('nd');
-			for (let j = 0; j < nodesinsideway.length - 1; j++) {
-				fromnode = getNodebyId(nodesinsideway[j].getAttribute("ref"));
-				tonode = getNodebyId(nodesinsideway[j + 1].getAttribute("ref"));
-				if (fromnode != null & tonode != null) {
-					let newEdge = new Edge(fromnode, tonode, wayid);
-					edges.push(newEdge);
-					totaledgedistance += newEdge.distance;
-				}
-			}
-		}
-		mode = selectnodemode;
-		showMessage("Click on start of route");
-	});
+    mapminlat = extent[1];
+    mapminlon = extent[0];
+    mapmaxlat = extent[3];
+    mapmaxlon = extent[2];
+
+    // Using a smaller margin to ensure the data matches the visual zoom
+    dataminlat = extent[1] + (extent[3] - extent[1]) * margin;
+    dataminlon = extent[0] + (extent[2] - extent[0]) * margin;
+    datamaxlat = extent[3] - (extent[3] - extent[1]) * margin;
+    datamaxlon = extent[2] - (extent[2] - extent[0]) * margin;
+
+    let OverpassURL = "https://overpass-api.de/api/interpreter?data=";
+
+    // This query uses the CityStrides logic to filter for "runnable" named streets only
+    let overpassquery = `[out:xml][timeout:30];
+(
+  way(${dataminlat},${dataminlon},${datamaxlat},${datamaxlon})
+  ["highway"]
+  ["name"]
+  ["highway"!~"^(bridleway|bus_guideway|bus_stop|busway|construction|corridor|cycleway|elevator|escape|footway|motorway|motorway_junction|motorway_link|path|platform|proposed|raceway|razed|rest_area|services|steps|via_ferrata)$"]
+  ["access"!~"^(no|customers|permit|private)$"]
+  ["indoor"!~"^(area|column|corridor|door|level|room|wall|yes)$"]
+  ["service"!~"^(drive-through|driveway|parking_aisle)$"]
+  ["foot"!~"no"]
+  ["area"!~"yes"]
+  ["motorroad"!~"yes"]
+  ["toll"!~"yes"];
+);
+(._;>;);
+out;`;
+
+    // Properly encode the query for the URL
+    let fullURL = OverpassURL + encodeURIComponent(overpassquery);
+
+    httpGet(fullURL, 'text', true, function (response) {
+        var parser = new DOMParser();
+        OSMxml = parser.parseFromString(response, "text/xml");
+        var XMLnodes = OSMxml.getElementsByTagName("node");
+        var XMLways = OSMxml.getElementsByTagName("way");
+
+        // SAFETY: If CityStrides logic returns 0 roads, don't freeze the UI
+        if (XMLways.length === 0) {
+            showMessage("No named roads found. Zoom out and try again!");
+            setTimeout(() => { mode = choosemapmode; showMessage("Zoom to area, then click here"); }, 3000);
+            return;
+        }
+
+        numnodes = XMLnodes.length;
+        numways = XMLways.length;
+
+        for (let i = 0; i < numnodes; i++) {
+            var lat = XMLnodes[i].getAttribute('lat');
+            var lon = XMLnodes[i].getAttribute('lon');
+            minlat = min(minlat, lat);
+            maxlat = max(maxlat, lat);
+            minlon = min(minlon, lon);
+            maxlon = max(maxlon, lon);
+        }
+
+        nodes = [];
+        edges = [];
+
+        for (let i = 0; i < numnodes; i++) {
+            var lat = XMLnodes[i].getAttribute('lat');
+            var lon = XMLnodes[i].getAttribute('lon');
+            var nodeid = XMLnodes[i].getAttribute('id');
+            let node = new Node(nodeid, lat, lon);
+            nodes.push(node);
+        }
+
+        // Parse ways into edges
+        for (let i = 0; i < numways; i++) {
+            let wayid = XMLways[i].getAttribute('id');
+            let nodesinsideway = XMLways[i].getElementsByTagName('nd');
+            for (let j = 0; j < nodesinsideway.length - 1; j++) {
+                let fromnode = getNodebyId(nodesinsideway[j].getAttribute("ref"));
+                let tonode = getNodebyId(nodesinsideway[j + 1].getAttribute("ref"));
+                if (fromnode != null && tonode != null) {
+                    let newEdge = new Edge(fromnode, tonode, wayid);
+                    edges.push(newEdge);
+                    totaledgedistance += newEdge.distance;
+                }
+            }
+        }
+
+        mode = selectnodemode;
+        showMessage("Click on start of route");
+    }, function (err) {
+        console.error("Overpass failed:", err);
+        showMessage("Overpass failed. Click here to retry");
+        mode = choosemapmode;
+    });
 }
 
 function showNodes() {
